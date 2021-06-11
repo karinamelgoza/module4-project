8ac7700a9b4e2f3a0f29bee8685e85ed4e975832
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-11 09:33:39 -0700

    part 2 solutions

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
+# opening file and assigning opened file  to log_file
 log_file = open("um-server-01.txt")
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):  # setting up function to work on log_file
+    for line in log_file:  # setting up loop on log_file
+        line = line.rstrip()  # removing trailing characters on each line
+        day = line[0:3]  # assigning day to equal items in index 0 to 3
+        if day == "Mon":  # if statement for when day is equal to "Mon"
+            print(line)  # if above is met the line will print
 
 
-sales_reports(log_file)
+sales_reports(log_file)  # calling the function
